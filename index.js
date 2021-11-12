ac3e6033ed816df02208f4ba3406acf025d4a533
Raul Valdez <rvaldez20@gmail.com>
2021-11-11 20:42:16 -0600

    Se configura el routing

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 require('dotenv').config({path: 'variables.env'});
 
-
+const routes = require('./routes/index.routes');
 const app =  express();
 
+//Routing
+app.use('/', routes())
 
 
+// Agrega el puerto
 app.listen(process.env.PORT, () => {
    console.log(`Server run on PORT ${process.env.PORT}`)
 })
---
+++ routes/index.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router =  express.Router();
+
+
+
+module.exports = function (){
+   router.get('/', (req, res) => {
+      res.send('Inicio')
+   });
+
+
+   router.get('/crear-cuenta', (req, res) => {
+      res.send('Crear Cuenta')
+   });
+
+   return router;
+}
