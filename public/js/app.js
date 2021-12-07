e19a5180bee4b973f248517030f14e9fa8ff9e68
Raul Valdez <rvaldez20@gmail.com>
2021-12-06 23:13:01 -0600

    Se configura el pin en el mapa, se tuvo que hacer con un pin personalizado y se config para que se mueva el pin

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,16 @@ import { OpenStreetMapProvider } from 'leaflet-geosearch';
 if( document.querySelector('#map') ) {
     const lat = 24.0277;
     const lgn = -104.653;
-    
     const map = L.map('map').setView([lat, lgn], 15);
+    const myIcon = L.icon({
+        iconUrl: 'img/pin_market_map.png',
+        iconSize: [35,35],
+        // iconAnchor: [22, 94],
+        // popupAnchor: [-3, -76],        
+        // shadowSize: [68, 95],
+        // shadowAnchor: [22, 94]
+    });
+    let marker;
     
     document.addEventListener('DOMContentLoaded', () => {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -25,9 +33,20 @@ if( document.querySelector('#map') ) {
             const provider = new OpenStreetMapProvider();
             provider.search({ query: e.target.value }).then((resultado) => {
                 console.log(resultado);
+        
+                // ubicamos el mapa en base a la busqueda
+                map.setView(resultado[0].bounds[0], 15);
                 
                 //Agregar el pin
-    
+                marker = new L.marker(resultado[0].bounds[0], {
+                    icon: myIcon,
+                    draggable: true,
+                    autoPan: true
+                })
+                .addTo(map)
+                .bindPopup(resultado[0].label)
+                .openPopup()
+
             })
     
     
