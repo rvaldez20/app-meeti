7fd20b5db11129faa22ae08243bf9a9782b7e8df
Raul Valdez <rvaldez20@gmail.com>
2021-12-06 21:44:30 -0600

    Se obtiene desde el input una geolocalizacion estimada usando el provider de liaflet-geoserch

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,43 @@
-var map = L.map('map').setView([24.505, 104], 13);
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+if( document.querySelector('#map') ) {
+    const lat = 24.0277;
+    const lgn = -104.653;
+    
+    const map = L.map('map').setView([lat, lgn], 15);
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    
+        // buscar la direccion
+        const  buscador = document.querySelector('#formbuscador');
+        buscador.addEventListener('input', buscarDireccion);
+    
+    })
+    
+    function buscarDireccion(e) {
+        if(e.target.value.length > 8) {
+            // console.log('Buscando') ;
+    
+            //utilizamos el prvader
+            const provider = new OpenStreetMapProvider();
+            provider.search({ query: e.target.value }).then((resultado) => {
+                console.log(resultado);
+                
+                //Agregar el pin
+    
+            })
+    
+    
+            // console.log(provider);
+        }
+    }
+    
+    
+    // L.marker([51.5, -0.09]).addTo(map)
+    //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //     .openPopup();
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+}  // if exite elemento map
